5207ab7617b36b18be6afa6e4966f257f5e99947
Junior Rabelo <rabelojunior105@gmail.com>
2020-09-09 23:44:13 -0300

    chalenge completed

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,17 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionRepositories from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepositories = await getCustomRepository(
+      TransactionRepositories,
+    );
+    const transactions = await transactionRepositories.findOne(id);
+    if (!transactions) {
+      throw new AppError('Transaction does not exists');
+    }
+    await transactionRepositories.remove(transactions);
   }
 }
 
